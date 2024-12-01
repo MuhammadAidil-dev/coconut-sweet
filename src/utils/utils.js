39585f125e0b1d966f1644cc47195e896bfe873b
MuhammadAidil-dev <muhammad.aidil09767@gmail.com>
2024-12-01 19:04:53 +0700

    feat: add ordered feature

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,4 +2,20 @@ const formattingPrice = (price) => {
   return price.toLocaleString('id-ID');
 };
 
-export { formattingPrice };
+const sendToWhatsapp = (orderMessage) => {
+  const { name, nomorHp, jumlah, alamat, variant, totalPrice } = orderMessage;
+  const phoneNumber = '6282280853894';
+
+  const message = `Halo Coconut Sweet, saya ingin memesan produk anda, berikut detail informasi : \n\n- Nama: ${name} \n- Nomor HP: ${nomorHp} \n- Jumlah: ${jumlah} \n- Varian: ${variant} \n- Alamat: ${alamat} \n\n Total Harga : Rp ${formattingPrice(
+    totalPrice
+  )}`;
+  const encodedMessage = encodeURIComponent(message);
+
+  // url whatsapp
+  const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+
+  // Buka WhatsApp di tab baru
+  window.open(whatsappURL, '_blank');
+};
+
+export { formattingPrice, sendToWhatsapp };
